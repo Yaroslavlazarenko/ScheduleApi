e00144c0164eafac2954be8060a53eb22c0d77a7
Yaroslavlazarenko <yaroslav.lazarenko.odessa@gmail.com>
2025-09-20 01:38:24 +0300

    edit pair time format to timeOnly

---
+++ ScheduleApi.Infrastructure/Migrations/20250919223705_edit_dateTimePair_to_timeOnly.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ScheduleBotApi.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class edit_dateTimePair_to_timeOnly : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<TimeOnly>(
+                name: "StartTime",
+                table: "Pairs",
+                type: "time without time zone",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone");
+
+            migrationBuilder.AlterColumn<TimeOnly>(
+                name: "EndTime",
+                table: "Pairs",
+                type: "time without time zone",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "StartTime",
+                table: "Pairs",
+                type: "timestamp with time zone",
+                nullable: false,
+                oldClrType: typeof(TimeOnly),
+                oldType: "time without time zone");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "EndTime",
+                table: "Pairs",
+                type: "timestamp with time zone",
+                nullable: false,
+                oldClrType: typeof(TimeOnly),
+                oldType: "time without time zone");
+        }
+    }
+}
