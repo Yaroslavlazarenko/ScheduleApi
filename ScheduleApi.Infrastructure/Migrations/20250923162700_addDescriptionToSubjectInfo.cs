3f522ba44fc338ceffb47078a8469a0a4acda468
Yaroslavlazarenko <yaroslav.lazarenko.odessa@gmail.com>
2025-09-23 19:29:04 +0300

    add subject info description

---
+++ ScheduleApi.Infrastructure/Migrations/20250923162700_addDescriptionToSubjectInfo.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ScheduleBotApi.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class addDescriptionToSubjectInfo : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "SubjectInfos",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "SubjectInfos");
+        }
+    }
+}
