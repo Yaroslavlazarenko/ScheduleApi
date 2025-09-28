d4230933ebca55386430276ef2a8199503f9e639
Yaroslavlazarenko <yaroslav.lazarenko.odessa@gmail.com>
2025-09-28 16:34:38 +0300

    fix teacher subject configuration relationship

---
+++ ScheduleApi.Infrastructure/Migrations/20250928133328_FixTeacherSubjectRelationship.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ScheduleBotApi.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixTeacherSubjectRelationship : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_TeacherSubjects_SocialMediaTypes_SocialMediaTypeId1",
+                table: "TeacherSubjects");
+
+            migrationBuilder.DropIndex(
+                name: "IX_TeacherSubjects_SocialMediaTypeId1",
+                table: "TeacherSubjects");
+
+            migrationBuilder.DropColumn(
+                name: "SocialMediaTypeId1",
+                table: "TeacherSubjects");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "SocialMediaTypeId1",
+                table: "TeacherSubjects",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TeacherSubjects_SocialMediaTypeId1",
+                table: "TeacherSubjects",
+                column: "SocialMediaTypeId1");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TeacherSubjects_SocialMediaTypes_SocialMediaTypeId1",
+                table: "TeacherSubjects",
+                column: "SocialMediaTypeId1",
+                principalTable: "SocialMediaTypes",
+                principalColumn: "Id");
+        }
+    }
+}
